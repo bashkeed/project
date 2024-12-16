88ee3b9c65023f2489fceff4f19ba29da2834457
zubair <bashirkwarra@gmail.com>
2024-12-16 12:57:56 +0100

    made change to the error message

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -35,7 +35,7 @@ const Login = () => {
           if (error.response && error.response.status === 401) {
             toast.error("Invalid email or password.");
           } else {
-            toast.error("An error occurred. Please try again.");
+            toast.error("Server error. Please try again.");
           }
 
           console.log(error);
