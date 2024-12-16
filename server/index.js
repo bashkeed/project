965fc140d7752f0216e192cdd88fb2d27034195e
zubair <bashirkwarra@gmail.com>
2024-12-16 13:05:11 +0100

    cors update for protocol

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["learnfly-nine.vercel.app", "http://localhost:5173"],
+    origin: ["https://learnfly-nine.vercel.app", "http://localhost:5173"],
   })
 );
 
