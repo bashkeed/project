b6743caf1b8b80cd8f82b61db5849da7cd1e7610
zubair <bashirkwarra@gmail.com>
2024-12-18 12:54:42 +0100

    fixed the quit confirmation dialog

--- client/src/components/QuitConfirmation.jsx
+++ client/src/components/QuitConfirmation.jsx
@@ -5,8 +5,11 @@ const QuitConfirmation = ({ isOpen, onClose, onConfirm }) => {
   if (!isOpen) return null;
 
   return (
-    <div className="fixed-top d-flex align-items-start justify-content-center bg-dark bg-opacity-50 vh-100">
-      <div className="bg-white p-4 rounded shadow-lg mt-5 w-50">
+    <div className="fixed-top d-flex align-items-center justify-content-center bg-opacity-50 vh-100">
+      <div
+        className="bg-white p-4 rounded shadow-lg w-100 mx-2 mx-md-5 mx-lg-0"
+        style={{ maxWidth: "500px" }}
+      >
         <h2 className="h5 mb-4">Are you sure you want to Submit?</h2>
         <div className="d-flex justify-content-between">
           <button onClick={onClose} className="btn btn-secondary mr-2">
